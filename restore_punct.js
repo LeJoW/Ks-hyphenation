a270552829bb909c9b332051cf741f87b62b17aa
LeJoW <elle.git.wilke@gmail.com>
2019-12-30 19:22:32 +0100

    Restore punct works

--- restore_punct.js
+++ restore_punct.js
@@ -1,29 +1,37 @@
 
-const CDK = "_cdk5555555cdk_";
-const BRUT = "ar=bre " + CDK + "=et aut=res";
-const ORIG = "Arbre =et Autres...";
+const CDK = "_cdk12332123443234554345665456776567887678998789009890cdk_";
+const BRUT = "ad do=mi=num cum tri=bu=la=rer cla=ma=vi " + CDK + " et in=ten=dit mi=hi";
+const ORIG = "Ad Dominum cum tribularer clamavi = et intendit mihi.";
 
 var orig_splited = ORIG.split(/(=)/g);
 
-BRUT.split(new RegExp("(" + CDK + ")", "g")).map((e, i) => {
+function recurse$restorePunct(object$hyphed, object$original) {
+    
+}
+
+console.log(BRUT.split(new RegExp("(" + CDK + ")", "g")).map((e, i) => {
+    let out = "";
     if (e !== CDK) {
         const lettersH = e.split('');
         const lettersO = orig_splited[i].split('');
-        id2 = 0;
-        lettersO.map((letter, id) => {
-            let letterH = lettersH[id2];
-            console.log(letter, id, letterH, id2);
-
-            if (letter.toLowerCase() === letterH) {
-                lettersH[id2] = letter;
-            } else if (letterH === '=') {
-                lettersH[id2] = '-' + letter;
-                id2--;
+        let idH = 0,
+            idO = 0;
+        for (let i = 0; i < Math.max(lettersH.length, lettersO.length) + 1; i++) {
+            let lH = lettersH[idH],
+                lO = lettersO[idO];
+            if (lO && lO.toLowerCase() === lH) {
+                idH++; idO++;
+                out += lO;
+            } else if (lH === '=') {
+                idH++;
+                out += '-';
+            } else if (lO) {
+                idO++;
+                out += lO;
             }
-
-            id2++;
-        });
-        console.log(lettersH, lettersO);
+        }
+    } else {
+        out += "=";
     }
-});
-
+    return out;
+}).join(''));
